30f86c991d02493305ba7caed68a4b93c8005a0a
Sinno.song <sinno.song@outlook.com>
2022-06-07 23:18:39 +0800

    添加AntDesign

--- Library.API.Web/Program.cs
+++ Library.API.Web/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddAntDesign();
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
