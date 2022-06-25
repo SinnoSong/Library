291e6a18130e762097fd3734111c01cd6ed9d5d6
Sinno.song <sinno.song@outlook.com>
2022-06-25 13:49:08 +0800

    feature : 修改userLayout和MainLayout

--- Library.API/Controllers/AuthenticateController.cs
+++ Library.API/Controllers/AuthenticateController.cs
@@ -11,7 +11,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Library.API.Controllers
