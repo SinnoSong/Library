59a73d291d44958b33d12e9687b0277a258ed6fe
Sinno.song <sinno.song@outlook.com>
2022-06-26 20:54:21 +0800

    feature : 通过样式调整页面显示

--- Library.Common/Models/RegisterUser.cs
+++ Library.Common/Models/RegisterUser.cs
@@ -9,11 +9,14 @@ namespace Library.Common.Models
         [JsonProperty("userName")]
         public string UserName { get; set; }
 
-        [JsonProperty("email")] [EmailAddress] public string Email { get; set; }
+        [JsonProperty("email")][EmailAddress] public string Email { get; set; }
 
         [JsonProperty("password")]
         [MinLength(6)]
         public string Password { get; set; }
+        [Required]
+        public string ConfirmPassword { get; set; }
+
 
         [JsonProperty("grade")] public byte Grade { get; set; }
     }
